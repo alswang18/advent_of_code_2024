d10de8fc03f35c3c7a122c8741e2b0d7268e0515
Alec Wang <alec.sy.wang@gmail.com>
2024-12-08 23:11:25 -0800

    update input text to use embed file

--- day1/src/main.zig
+++ day1/src/main.zig
@@ -5,27 +5,6 @@ pub fn main() !void {
     const allocator = arena.allocator();
     defer arena.deinit();
 
-    const stdin = std.io.getStdIn().reader();
-    const stdout = std.io.getStdOut().writer();
-    var buf: [1024]u8 = undefined;
-
-    try stdout.print("Please enter your file path:\n", .{});
-    var fp: []u8 = undefined;
-    if (try stdin.readUntilDelimiterOrEof(&buf, '\n')) |l| {
-        fp = l;
-    } else {
-        std.debug.print("No valid input\n", .{});
-        return;
-    }
-
-    // read input.txt
-    var file = std.fs.cwd().openFile(fp, .{}) catch |err| {
-        std.debug.print("Error opening file: {}\n", .{err});
-        return;
-    };
-
-    defer file.close();
-
     // create an array list of integers
     var left = std.ArrayList(i32).init(allocator);
     var right = std.ArrayList(i32).init(allocator);
@@ -34,12 +13,12 @@ pub fn main() !void {
         right.deinit();
     }
 
-    var buf_reader = std.io.bufferedReader(file.reader());
-    var in_stream = buf_reader.reader();
-
+    const input_text = @embedFile("input.txt");
+    var it = std.mem.tokenize(u8, input_text, "\n");
     // read each line
-    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+    while (it.next()) |line| {
         var iter = std.mem.split(u8, line, "   ");
+
         const left_num = try std.fmt.parseInt(i32, iter.next().?, 10);
         const right_num = try std.fmt.parseInt(i32, iter.next().?, 10);
         try left.append(left_num);
